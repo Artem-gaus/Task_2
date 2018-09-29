53c473cb00582865dace876fea6f0e79dc5e7fa2
Artem-gaus <gausin.artem@gmail.com>
2018-09-29 16:14:33 +0300

    Add Insert method, Node model

---
+++ Task_2/BinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class BinaryTree
+    {
+        public Node Insert(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.Value = value;
+            }
+            else if (value < root.Value)
+                root.LeftNode = Insert(root.LeftNode, value);
+            else
+                root.RightNode = Insert(root.RightNode, value);
+
+            return root;
+        }
+    }
+}
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -12,6 +12,16 @@ namespace Task_2
         //last step is add DLL lib and move there
         static void Main(string[] args)
         {
+            Node root = null;
+            BinaryTree tree = new BinaryTree();
+            int[] fill = new int[] {10, 3, 15, 28, 4, -15, 2, 56 }; 
+
+            for (int i = 0; i < fill.Length; i++)
+            {
+                root = tree.Insert(root, fill[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
